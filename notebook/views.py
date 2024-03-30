c4b73b693ed4a1742f89c6e651eea304e95d37b4
Kalpit-prabhat <prabhatkalpit@gmail.com>
2024-03-30 16:09:29 +0530

    added notebook view

--- notebook/views.py
+++ notebook/views.py
@@ -1,6 +1,16 @@
+from django.http import Http404
 from django.shortcuts import render
+from .models import *
+
+VALID_IDS = ['drylab','design']
 
 # Create your views here.
+def notebook(request, id):
+    if id in VALID_IDS:
+        notes = Notes.objects.filter(notebook=id).all().order_by('created').reverse()
+    else:
+        raise Http404('Notebook does not exist!')
+    return render(request, "notebook/notebook.html", {'notes': notes})
 
 def index(request):
     return render(request, 'notebook/index.html')
