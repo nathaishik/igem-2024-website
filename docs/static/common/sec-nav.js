fa92982a43f4671af7b3dea801c7a5d8fcc0e183
nathaishik <nathaishik@gmail.com>
2024-06-13 21:15:10 +0530

    forgot to update static files last commit

--- docs/static/common/sec-nav.js
+++ docs/static/common/sec-nav.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const len = url_array.length;
   let links = document.querySelectorAll('#pri-nav a, #sec-nav a');
   let url_str = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/';
+  console.log(window.location.port);
   for (let i = 0; i < len; i++) {
     for (let a of links) {
       if (a.href === url_str + url_array[i] + '/' || a.href === url_str + url_array[i]) {
