4e8677cd412e72d9ea1b2bd39a7bf0fdf0dea7fd
nathaishik <nathaishik@gmail.com>
2024-06-16 16:51:02 +0530

    Added 1 test for admin view

--- notebook/test_views.py
+++ notebook/test_views.py
@@ -250,3 +250,14 @@ class NotebookTestCaseViews(TestCase):
                         })
         self.assertEqual(response.status_code, 302)
         self.assertFalse(bool(u.notes.all()[0].file))
+
+    def test_invalid_user_admin_access(self):
+        """This should return a 403 status code."""
+        u3 = User.objects.create_user(username="user3", password="password3")
+        u3.verified = True
+        u3.position = 2
+        u3.save()
+        c = Client()
+        c.login(username="user3", password="password3")
+        response = c.get(reverse('notebook:admin'))
+        self.assertEqual(response.status_code, 403)
