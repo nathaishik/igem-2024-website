f30f8cf1fec37b27871b7e8da68728005e86c3f5
nathaishik <nathaishik@gmail.com>
2024-06-18 00:39:45 +0530

    Added allowed host

--- wiki/settings_prod.py
+++ wiki/settings_prod.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.koyeb.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'iisc-bengaluru-iisc-bengaluru-5dfbea53.koyeb.app']
 
 
 # Application definition
